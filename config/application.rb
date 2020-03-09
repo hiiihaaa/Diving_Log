c8b53a6b47622839fd03453a5585e70b55f6991b
hiiihaaa <snshiiihaaa@gmail.com>
2020-03-10 00:31:07 +0900

    initial_commit

---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module DivingLog
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper      false 
+    end
+  end
+end
